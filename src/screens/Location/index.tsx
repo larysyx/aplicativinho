ae4b1e18b9f47778450c77573774ef55b0f7cb1c
larysyx <lauragabriela.lemos3@gmail.com>
2023-06-20 15:22:53 -0300

    19.06

--- src/screens/Location/index.tsx
+++ src/screens/Location/index.tsx
@@ -1,13 +1,14 @@
 import { Text, View } from 'react-native';
-import MapViewDirections from 'react-native-maps-directions';
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
-import { API_GOOGLE } from '@env'
 import * as Location from 'expo-location';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import MapView, { Region, Marker, Polyline } from "react-native-maps";
 import { styles } from './styles';
 import { ReloadInstructions } from 'react-native/Libraries/NewAppScreen';
 import { colors } from '../../styles/colors';
+import MapViewDirections from 'react-native-maps-directions';
+import { GooglePlaceData, GooglePlaceDetail, GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { API_GOOGLE } from '@env'
+import { MaterialCommunityIcons, Entypo } from '@expo/vector-icons';
 type ICoords = {
     latitude: number
     longitude: number
@@ -19,6 +20,8 @@ export function LocationScreen() {
     const [region, setRegion] = useState<Region>()
     const [marker, setMarker] = useState<Region[]>()
     const [coords, setCoords] = useState<ICoords[]>([])
+    const [destination, setDestination] = useState<Region | null>(null)
+    const mapRef = useRef<MapView>(null)
 
     useEffect(() => {
         let subcription: Location.LocationSubscription
@@ -40,7 +43,7 @@ export function LocationScreen() {
                 latitude: location.coords.latitude,
                 longitude: location.coords.longitude,
                 latitudeDelta: 0.009,
-                longitudeDelta: 0.009   
+                longitudeDelta: 0.009
             }])
             subcription = await Location.watchPositionAsync({
                 accuracy: Location.LocationAccuracy.High,
@@ -50,32 +53,89 @@ export function LocationScreen() {
                 setCoords((prevState) => [...prevState, location.coords])
             });
         })();
-        return () => { 
+        return () => {
             if (subcription) {
                 subcription.remove()
             }
         }
     }, []);
 
+    async function handleDestination(data: GooglePlaceData, details: GooglePlaceDetail | null) {
+        if (details) {
+            setDestination({
+                latitude: details?.geometry.location.lat,
+                longitude: details?.geometry.location.lng,
+                latitudeDelta: 0.004,
+                longitudeDelta: 0.004
+            })
+            if (marker) {
+                setMarker([...marker, {
+                    latitude: details?.geometry.location.lat,
+                    longitude: details?.geometry.location.lng,
+                    latitudeDelta: 0.004,
+                    longitudeDelta: 0.004
+                }])
+            }
+        }
+    }
+
     let text = 'Localizando..';
     if (errorMsg) {
         text = errorMsg
     } else if (location) {
         text = JSON.stringify(location);
+    } else if (destination) {
+        text = JSON.stringify(destination);
     }
     return (
         <View style={styles.container}>
             {region ? (
-                <MapView region={region} style={styles.map} showsUserLocation={true}>
-                    {marker && marker.map((i)=> (
-                        <Marker key={i.latitude} coordinate={i} />
-                    ))}
-                    {coords && <Polyline
-                    coordinates={coords}
-                    strokeColor={colors.azul}
-                    strokeWidth={7}
-                    />}
-                </MapView>
+                <>
+                    <GooglePlacesAutocomplete
+                        styles={{ container: styles.searchContainer, textInput: styles.searchInput }}
+                        placeholder="Destino"
+                        fetchDetails={true}
+                        GooglePlacesDetailsQuery={{ fields: "geometry" }}
+                        enablePoweredByContainer={false}
+                        query={{
+                            key: API_GOOGLE,
+                            language: 'pt-BR'
+                        }}
+                        onFail={setErrorMsg}
+                        onPress={handleDestination}
+                    />
+                    <MapView region={region} style={styles.map} showsUserLocation={true}>
+                        {marker && marker.map((i) => (
+                            <Marker key={i.latitude} coordinate={i}>
+                                <Entypo name="location-pin" size={48} color={colors.black} />
+                            </Marker>
+                        ))}
+                        {coords && <Polyline
+                            coordinates={coords}
+                            strokeColor={colors.azul}
+                            strokeWidth={7}
+                        />}
+                        {destination && (
+                            <MapViewDirections
+                                origin={region}
+                                destination={destination}
+                                apikey={API_GOOGLE}
+                                strokeColor={colors.black}
+                                strokeWidth={6}
+                                onReady={(result) => {
+                                    mapRef.current?.fitToCoordinates(result.coordinates, {
+                                        edgePadding: {
+                                            top: 24,
+                                            bottom: 24,
+                                            left: 24,
+                                            right: 24
+                                        }
+                                    })
+                                }}
+                            />
+                        )}
+                    </MapView>
+                </>
             ) : (
                 <Text style={styles.paragraph}>{text}</Text>
             )}
